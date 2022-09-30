ce323745449569899ab90f1a6eb4938330b8e3aa
Vishal Bhargava <vishuji215@gmail.com>
2022-09-30 23:07:34 +0530

    use case added

---
+++ com/hrms/useCase/ApproveLeave.java
@@ -0,0 +1,5 @@
+package com.hrms.useCase;
+
+public class ApproveLeave {
+
+}
---
+++ com/hrms/useCase/UpdateProfileEmp.java
@@ -0,0 +1,10 @@
+package com.hrms.useCase;
+
+public class UpdateProfileEmp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
