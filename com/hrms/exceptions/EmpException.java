6aebbf04ee80ea3592144f27c3b17b0459bdf166
Vishal Bhargava <vishuji215@gmail.com>
2022-10-01 12:08:01 +0530

    exception classes added

---
+++ com/hrms/exceptions/EmpException.java
@@ -0,0 +1,12 @@
+package com.hrms.exceptions;
+
+public class EmpException extends Exception{
+	
+		public EmpException() {
+			super();
+		}
+		
+		public EmpException(String message) {
+			super(message);
+		}
+}
